689e9f851d6e9a702379406533eb421c36c98d52
Evgenii Dobrovolets <doktor-dell@mail.ru>
2022-01-06 16:54:21 +0300

    Initial Commit

---
+++ FirstApp HomeWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstApp HomeWork
+//
+//  Created by Евгений Доброволец on 06.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
