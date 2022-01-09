5e13fa8cd1c1c9cddc87c3ed9cd8fd0b5c9d08f5
EvgeniiDB <doktor-dell@mail.ru>
2022-01-09 21:17:43 +0300

    screen for enter

--- FirstApp HomeWork/ViewController.swift
+++ FirstApp HomeWork/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func didTapOnButton(_ sender: UIButton) {
+        
+        print("Hello Bro!")
+        
+    }
+    
+    @IBAction func tapRegButton(_ sender: UIButton) {
+        print("Введите номер телефона")
+    }
+    
+    @IBAction func tapForgPassButton(_ sender: UIButton) {
+        print("Напомнить пароль")
+    }
+    
 
 }
 
