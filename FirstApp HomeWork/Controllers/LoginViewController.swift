a1476164de6e75d9e89f01bd53aa4c581fac57a4
EvgeniiDB <doktor-dell@mail.ru>
2022-02-06 01:08:41 +0300

    news in processed

--- FirstApp HomeWork/Controllers/LoginViewController.swift
+++ FirstApp HomeWork/Controllers/LoginViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
     
     @IBOutlet weak var loginTextField: UITextField!
     
@@ -47,7 +47,7 @@ class ViewController: UIViewController {
     
 }
 
-private extension ViewController {
+private extension LoginViewController {
     func setupTextFields() {
         loginTextField.keyboardType = .emailAddress
         passwordTextField.keyboardType = .numberPad
---
+++ FirstApp HomeWork/Model/News.swift
@@ -0,0 +1,15 @@
+//
+//  News.swift
+//  FirstApp HomeWork
+//
+//  Created by Евгений Доброволец on 05.02.2022.
+//
+
+import Foundation
+import UIKit
+
+struct News {
+    
+    var newsText: String
+    var newsImage: UIImage?
+}
