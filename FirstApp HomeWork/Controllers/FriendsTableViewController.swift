2aaa04fd4d5a9542c9f6bef5ae6bdf146b0cae34
EvgeniiDB <doktor-dell@mail.ru>
2022-02-06 21:21:43 +0300

    Home Work less 6

--- FirstApp HomeWork/Controllers/FriendsTableViewController.swift
+++ FirstApp HomeWork/Controllers/FriendsTableViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class FriendsTableViewController: UITableViewController {
     
+    
+    @IBOutlet var friendsTableView: UITableView!
+    
     var friendsArray = [User]()
     var savedObject: Any?
     
--- FirstApp HomeWork/Controllers/NewsViewController.swift
+++ FirstApp HomeWork/Controllers/NewsViewController.swift
@@ -9,21 +9,91 @@ import UIKit
 
 class NewsViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView! {
+        didSet {
+            self.collectionView.dataSource = self
+            self.collectionView.delegate = self
+        }
+    }
+    
+    private let reuseIdentifier = "NewsCell"
+    
+    private var newsArray = [News]()
+    
+    private func setupNews() -> [News] {
+        
+        var resultArray = [News]()
+        
+        let newsOne = News(newsText: "По итогам первого дня официальных тестов MotoGP на трассе Сепанг итоговый протокол возглавили два пилота Aprilia. Частично это не есть неожиданность, поскольку благодаря концессиям у обоих пилотов была возможность выехать на малазийский трек раньше остальных. Однако результат лидера Алейша Эспаргаро всё равно впечатляет, ведь он проехал лучший круг за 1’58.371 и уступил всего одну десятую рекорду трассы. И это не его максимум.", newsImage: UIImage(named: "n_1"))
+        
+        resultArray.append(newsOne)
+        
+        let newsTwo = News(newsText: "Команда Gresini Racing официально сообщила о печальном известии. К сожалению, чемпион мира 125сс (1985, 1987) и руководитель команды MotoGP Фаусто Грезини (основана в 1997-м) скончался после почти двухмесячной госпитализации из-за осложнений, вызванных коронавирусом.", newsImage: UIImage(named: "n_2"))
+        
+        resultArray.append(newsTwo)
+        
+        let newsThree = News(newsText: "12 января Маверику Виньялесу, одной из самых ярких персон современного MotoGP, исполнилось 27 лет. Несмотря на столь юный возраст, ТопГан успел повыступать в цветах трёх разных производителей, и в сезоне-2022 он продолжит выступать за Aprilia. Маверика всегда считали одним из самых одарённых, однако нестабильные результаты ещё не позволили Маку побороться за престижную корону.", newsImage: UIImage(named: "n_3"))
+        
+        resultArray.append(newsThree)
+        
+        let newsFour = News(newsText: "Тесты в Сепанге — возможность для ощущения сладостного предвкушения предстоящего сезона MotoGP. Хотя для гонщиков и производителей тесты в основном является возможностью для тонкой настройки мотоцикла, на котором они проведут сезон-2022. Пилот Suzuki Алекс Ринс доволен тем, как провёл первый день малазийских тестов, заняв третье место, хотя, как обычно, ещё есть куда расти.", newsImage: UIImage(named: "n_4"))
+        
+        resultArray.append(newsFour)
+        
+        return resultArray
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        newsArray = setupNews()
+        
+        collectionView.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+        
+        if let flowLayout = collectionView?.collectionViewLayout as? UICollectionViewFlowLayout {
+            flowLayout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+        }
+        
+        collectionView.register(UINib(nibName: "NewsCollectionViewCell", bundle: nil),
+                                forCellWithReuseIdentifier: reuseIdentifier)
+       
+    }
+}
+
+extension NewsViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return newsArray.count
     }
-    */
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as? NewsCollectionViewCell else { return UICollectionViewCell()}
+        cell.configure(news: newsArray[indexPath.item])
+        
+        return cell
+    }
+}
 
+extension NewsViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        return CGSize(width: 10, height: 10)
+    }
+    
 }
